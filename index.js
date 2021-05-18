031d9c0a3bcb663099c7002c169d1170d5a558d3
fidellim <lim.fidel@gmail.com>
2021-05-18 04:29:05 +0400

    toggle event added

--- index.js
+++ index.js
@@ -1,67 +1,49 @@
 window.addEventListener("DOMContentLoaded", () => {
+	let openedAccordionItem = null;
 	const questions = document.querySelectorAll(".question-container");
 	questions.forEach((question) =>
-		question.addEventListener("click", () => {
-			//get the next element of questionContainer (answerContainer)
-			const sibling = question.nextElementSibling;
-			const siblingStyle = getComputedStyle(sibling);
+		question.addEventListener("click", (event) => {
+			const clickedAccordionItem = event.currentTarget;
 
-			//get last element of questionContainer (IMG (arrow down))
-			const lastChild = question.lastElementChild;
-			const lastChildStyle = getComputedStyle(lastChild);
-
-			const firstChild = question.firstElementChild;
-			const firstChildStyle = getComputedStyle(firstChild);
-
-			lastChild.style.transform =
-				lastChildStyle.getPropertyValue("transform") === "none"
-					? "rotate(180deg)"
-					: "none";
-			lastChild.style.transition = "all 0.3s ease-in";
-
-			firstChild.style.fontWeight =
-				firstChildStyle.getPropertyValue("font-weight") === "400"
-					? "700"
-					: "400";
-			sibling.style.margin = "0 0 1rem 0";
-			sibling.style.padding = "0 1rem 0 0";
-			sibling.style.color = "hsl(240, 6%, 50%)";
-			sibling.style.display =
-				siblingStyle.getPropertyValue("display") === "none" ? "block" : "none";
+			if (clickedAccordionItem !== openedAccordionItem) {
+				toggleElement(clickedAccordionItem);
+				toggleElement(openedAccordionItem);
+				openedAccordionItem = clickedAccordionItem;
+			} else {
+				toggleElement(clickedAccordionItem);
+				openedAccordionItem = null;
+			}
 		})
 	);
 });
 
-// window.addEventListener("DOMContentLoaded", (event) => {
-// 	let openedAnswerAccordion = null;
-// 	const questions = document.querySelectorAll(".question-container");
-// 	questions.forEach((question) =>
-// 		question.addEventListener("click", () => {
-// 			const clickedAnswerAccordion = event.currentTarget;
-// 			const sibling = question.nextElementSibling;
-// 			const siblingStyle = getComputedStyle(sibling);
+function toggleElement(item) {
+	if (item === null) {
+		return;
+	}
 
-// 			if (clickedAnswerAccordion !== openedAnswerAccordion) {
-// 				sibling.style.margin = "0 0 1rem 0";
-// 				sibling.style.padding = "0 1rem 0 0";
-// 				sibling.style.color = "hsl(240, 6%, 50%)";
-// 				sibling.style.display =
-// 					siblingStyle.getPropertyValue("display") === "none"
-// 						? "block"
-// 						: "none";
+	//get the next element of questionContainer (answerContainer)
+	const sibling = item.nextElementSibling;
+	const siblingStyle = getComputedStyle(sibling);
 
-// 				openedAnswerAccordion = clickedAnswerAccordion;
-// 			} else {
-// 				sibling.style.margin = "0 0 1rem 0";
-// 				sibling.style.padding = "0 1rem 0 0";
-// 				sibling.style.color = "hsl(240, 6%, 50%)";
-// 				sibling.style.display =
-// 					siblingStyle.getPropertyValue("display") === "none"
-// 						? "block"
-// 						: "none";
+	//get last element of questionContainer (IMG (arrow down))
+	const lastChild = item.lastElementChild;
+	const lastChildStyle = getComputedStyle(lastChild);
 
-// 				openedAnswerAccordion = null;
-// 			}
-// 		})
-// 	);
-// });
+	const firstChild = item.firstElementChild;
+	const firstChildStyle = getComputedStyle(firstChild);
+
+	lastChild.style.transform =
+		lastChildStyle.getPropertyValue("transform") === "none"
+			? "rotate(180deg)"
+			: "none";
+	lastChild.style.transition = "all 0.3s ease-in";
+
+	firstChild.style.fontWeight =
+		firstChildStyle.getPropertyValue("font-weight") === "400" ? "700" : "400";
+	sibling.style.margin = "0 0 1rem 0";
+	sibling.style.padding = "0 1rem 0 0";
+	sibling.style.color = "hsl(240, 6%, 50%)";
+	sibling.style.display =
+		siblingStyle.getPropertyValue("display") === "none" ? "block" : "none";
+}
